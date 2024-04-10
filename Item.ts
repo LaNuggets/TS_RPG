bd25d55361eff79e9041e4db239f1823a1b1a215
Stan <stanislas.ibrahim@ynov.com>
2024-04-10 14:40:01 +0200

    modification de item

--- Item.ts
+++ Item.ts
@@ -5,11 +5,17 @@ export default class Item {
     healthRestitution: number;
     manaRestitution: number;
 
+
     constructor(name: string, capacity: string, ressusciteHealth: number, healthRestitution: number, manaRestitution: number){
         this.name = name;
         this.capacity = capacity;
         this.ressusciteHealth = ressusciteHealth;
         this.healthRestitution = healthRestitution;
         this.manaRestitution = manaRestitution;
+    }
+
+    addItem(itemsInInventory : Item[]){
+        itemsInInventory.push(this);
+        console.log(`You add ${this.name} in your inventory !`)
     }
 }
