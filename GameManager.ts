b461cedd8f75ce20bc260d2d188bfff9a681e2cc
Stan <stanislas.ibrahim@ynov.com>
2024-04-10 08:28:28 +0200

    début implémentation dans la classe gamemanager

--- GameManager.ts
+++ GameManager.ts
@@ -1,5 +1,9 @@
-import Character from "./Character.ts";
+import Menu from "./Menu.ts";
 
 class GameManager {
-    
+    startGame(){
+        const menu = new Menu ();
+        menu.characterChoice();
+    }
 }
+
