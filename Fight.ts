b88165baea7f57d3af6f1c60ce9b335a5b49dcde
Cninoo <nino.carlotti@hotmail.fr>
2024-04-10 08:04:13 +0200

    enemieTeam

--- Fight.ts
+++ Fight.ts
@@ -20,4 +20,21 @@ export default class Fight {
         });
         return arr
     }
+
+    enemieTeam(): Character[] {
+        const arr : Character[] = [];
+        const enemies = this.fighters.filter(fighter => !fighter.team);
+        enemies.forEach(enemy => {
+            arr.push(enemy);
+        });
+        const newTeam: Character[] = [];
+        while(newTeam.length < 3 && arr.length > 0) {
+        const random = Math.floor(Math.random() * arr.length);
+        const randomEnemy = arr.splice(random, 1)[0];
+        newTeam.push(randomEnemy);
+        }
+        return newTeam;
+    }
 }
+
+console.log('\x1b[38;5;50m%s\x1b[0m' , 'salut')
